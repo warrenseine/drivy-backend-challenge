df4b2fd735f61bd921923b757c69888a2555f1a4
Warren Seine <warren.seine@aerys.in>
2016-11-06 19:48:02 +0100

    Solve level 1.

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,16 @@
 require "json"
+require "date"
 
-# your code
+data = JSON.parse(File.read("data.json"))
+result = { "rentals" => [] }
+data["rentals"].each { |rental|
+    car = data["cars"].detect { |car| car["id"] == rental["car_id"] }
+    duration = (Date.parse(rental["end_date"]) - Date.parse(rental["start_date"])).to_i + 1
+    price = rental["distance"] * car["price_per_km"] + duration * car["price_per_day"]
+
+    result["rentals"] += [{
+        "id" => rental["id"],
+        "price" => price
+    }]
+}
+File.write("output.json", JSON.pretty_generate(result) + "\n")
